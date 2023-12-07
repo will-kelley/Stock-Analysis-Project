54b7f8cc53978fec1aaa66a0e26e3bda4ab9a019
Will Kelley <148305668+will-kelley@users.noreply.github.com>
2023-12-06 20:41:30 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-from stock_analysis import fetch_stock_data, calculate_moving_averages, plot_stock_data
+from stock_analysis import (fetch_stock_data, calculate_moving_averages,
+                            plot_stock_data, fit_trendlines_single, fit_trendlines_high_low)
 from datetime import datetime
 
 
@@ -13,13 +14,21 @@ def main():
     bars = fetch_stock_data(ticker, datetime(start_year, start_month, start_day),
                             datetime(end_year, end_month, end_day))
 
-    # Ask user if they want to include moving averages
+    # Ask user if they want to include moving averages and volume bars
     include_ma = input("Include Moving Averages? (yes/no): ").lower() == 'yes'
     include_volume = input("Include Volume Bars? (yes/no): ").lower() == 'yes'
+
+    # Ask user if they want to include trend line analysis
+    include_trend_lines = input("Include Trend Line Analysis? (yes/no): ").lower() == 'yes'
 
     # Calculate moving averages for the stock data if selected
     if include_ma:
         bars = calculate_moving_averages(bars)
+
+    # Perform trend line analysis if selected
+    if include_trend_lines:
+        support_coefs, resist_coefs = fit_trendlines_single(bars['close'].to_numpy())
+        # Add code here to use these coefficients in your plot, if needed
 
     # Plot the stock data along with selected features
     plot_stock_data(bars, ticker, include_ma, include_volume)
@@ -27,4 +36,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
